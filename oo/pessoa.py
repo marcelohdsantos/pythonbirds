a344a00c9c1640c3dddccb851b74385ceae3a75a
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2022-05-11 15:51:27 -0400

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self.nome}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Marcelo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
