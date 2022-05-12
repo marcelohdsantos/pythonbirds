989ef847b27c7959974bacdf41cba6ca155e6417
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2022-05-12 10:16:28 -0400

    criado e removido atributo dinâmico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,19 @@ class Pessoa:
         return f'Olá {self.nome}'
 
 if __name__ == '__main__':
-    marcelo = Pessoa(nome='Marcelo')
-    maria = Pessoa(marcelo, nome='Maria')
-    for filho in maria.filhos:
-        print(filho.nome)
+  marcelo = Pessoa(nome='Marcelo')
+  andre = Pessoa(nome='André')
+
+  maria = Pessoa({andre, marcelo}, nome='Maria')
+  maria = Pessoa(andre, nome='Maria')
+
+  for filho in maria.filhos:
+      print(filho.nome)
+
+marcelo.sobrenone = 'Dos Santos'
+del maria.filhos
+del marcelo.sobrenone
+print(maria.__dict__)
+print(marcelo.__dict__)
+
+
