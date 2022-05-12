06cb5390661eb6ca9babf01c55f3d4015e4d4d1d
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2022-05-12 08:53:07 -0400

    Criados atributos complexos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,13 @@
 class Pessoa:
-    def __init__(self, nome=None):
+    def __init__(self, *filhos, nome=None):
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {self.nome}'
 
 if __name__ == '__main__':
-    p = Pessoa('Marcelo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    marcelo = Pessoa(nome='Marcelo')
+    maria = Pessoa(marcelo, nome='Maria')
+    for filho in maria.filhos:
+        print(filho.nome)
