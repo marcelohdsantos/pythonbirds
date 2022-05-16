71ea0441a2d7d6a112ea7da4ea84fa81177eee29
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2022-05-16 15:09:52 -0400

    Implementada a direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,6 +7,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {self.nome}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def atributo_de_classe_e_nome(cls):
+        return f'{cls}  {cls.olhos} -> olhos.'
+
 if __name__ == '__main__':
   marcelo = Pessoa(nome='Marcelo')
   andre = Pessoa(nome='André')
@@ -26,5 +34,8 @@ print(andre.__dict__)
 print(marcelo.olhos)
 print(maria.__dict__)
 print(marcelo.__dict__)
+print(marcelo.metodo_estatico())
+print(Pessoa.metodo_estatico())
+print(Pessoa.atributo_de_classe_e_nome(), marcelo.atributo_de_classe_e_nome())
 
 
